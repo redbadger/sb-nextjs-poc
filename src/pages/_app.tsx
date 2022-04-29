4b6749d7dad73b3c3334395abff05e1b0fd023df
Pedro Martin <pataruco@gmail.com>
2022-04-29 13:32:17 +0100

    Add state persistance

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import '../styles/globals.css';
 
 import { Provider } from 'react-redux';
 import type { AppProps } from 'next/app';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from '../app/store';
+import store, { persistor } from '../app/store';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   );
 }
